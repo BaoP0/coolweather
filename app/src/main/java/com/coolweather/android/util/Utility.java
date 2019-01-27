bcd34baf2c7693e0207af9e6632b11e49f3dabb9
BaoPo <htb88168@gmail.com>
2019-01-27 20:30:45 +0800

    添加了获取省市县的数据接口

--- app/src/main/java/com/coolweather/android/util/Utility.java
+++ app/src/main/java/com/coolweather/android/util/Utility.java
@@ -3,6 +3,8 @@ package com.coolweather.android.util;
 import android.text.TextUtils;
 
 
+import com.coolweather.android.db.City;
+import com.coolweather.android.db.County;
 import com.coolweather.android.db.Province;
 
 import org.json.JSONArray;
@@ -36,4 +38,43 @@ public class Utility {
         return false;
     }
 
+    public static boolean handleCityResponse(String response, int provinceId) {
+        if (!TextUtils.isEmpty(response)) {
+            try {
+                JSONArray allCities = new JSONArray(response);
+                for (int i = 0; i < allCities.length(); i++) {
+
+                    JSONObject cityObject = allCities.getJSONObject(i);
+                    City city = new City();
+                    city.setCityName(cityObject.getString("name"));
+                    city.setCityCode(cityObject.getInt("id"));
+                    city.setProvinceId(provinceId);
+                    city.save();
+                }
+                return true;
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    public static boolean handleCountyResponse(String response, int cityId) {
+        try {
+            JSONArray allCounties = new JSONArray(response);
+            for (int i = 0; i < allCounties.length(); i++) {
+                JSONObject countyObject = allCounties.getJSONObject(i);
+                County county = new County();
+                county.setCountyName(countyObject.getString("name"));
+                county.setId(countyObject.getInt("id"));
+                county.setCityId(cityId);
+                county.setWeatherId(countyObject.getString("weather_id"));
+                county.save();
+            }
+            return true;
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
 }
